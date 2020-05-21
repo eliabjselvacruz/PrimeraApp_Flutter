bc784af94b2312d08b73326e6d1f7cb1e47cf756
eliab51 <eliabjselvacruz51@gmail.com>
2020-05-20 21:59:46 -0600

    Agregando componentes widgets y paquetes externos.

--- primeraapp/lib/main.dart
+++ primeraapp/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(Center(
-    child: Text(
-      '¡Hola Mundo XD!',
-      textDirection: TextDirection.ltr,
-    ),
-  ));
+import 'package:english_words/english_words.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    final wordPair = new WordPair.random();
+    return MaterialApp(
+      title: 'Bienvenido a Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bienvenido a Flutter'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        )
+      ),
+    );
+  }
 }
