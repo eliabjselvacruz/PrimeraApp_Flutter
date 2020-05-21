909d5d0f561f9fa8f9efbc45b8ac37f99e833763
eliab51 <eliabjselvacruz51@gmail.com>
2020-05-21 00:31:03 -0600

    Usando Widget tipo StateFul y creando Widget ListView.

--- primeraapp/lib/main.dart
+++ primeraapp/lib/main.dart
@@ -5,17 +5,47 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    final wordPair = new WordPair.random();
     return MaterialApp(
       title: 'Bienvenido a Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bienvenido a Flutter'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        )
-      ),
+      home: RandomWords(),
     );
   }
+}
+
+class RandomWords extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords>{
+  final _suggestions = <WordPair>[];
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de nombres aleatorios'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      itemBuilder: (context, i){
+        if(i>=_suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[i]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(pair.asPascalCase)
+    );
+  }
+
 }
